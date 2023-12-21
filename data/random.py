94980db93d52a39e36bebb23ab999460724cdbdd
LordReaps <BovenkoAndrey@gmail.com>
2023-12-21 09:42:02 +0300

    добавил рандом картинки(не работает) пока ждем нормальных названий файлов

--- data/random.py
+++ data/random.py
@@ -1,4 +1,16 @@
+import random
+import os
+
 def RandomCard():
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
-    c = random.randint(1, 10)
+    List = []
+    while len(List)<=3:
+        i = random.choice(os.listdir("./ai_test/taro_card_ai"))
+        if i in List:
+            i = random.choice(os.listdir("./ai_test/taro_card_ai"))
+        else:
+            List.append(i)
+
+    a = List[0]
+    b = List[1]
+    c = List[2]
+    return [a, b, c]
