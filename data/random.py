2b4314f23b4059785295fc9951c4cf5c42aaa6b8
LordReaps <BovenkoAndrey@gmail.com>
2023-12-22 10:02:15 +0300

    картинка читается + список карт в словаре

--- data/random.py
+++ data/random.py
@@ -5,9 +5,9 @@ import os
 def RandomCard():
     List = []
     while len(List)<=3:
-        i = random.choice(os.listdir("./ai_test/tarot_for_Andrey"))
+        i = random.choice(os.listdir("static/tarot_for_Andrey"))
         if i in List:
-            i = random.choice(os.listdir("./ai_test/tarot_for_Andrey"))
+            i = random.choice(os.listdir("static/tarot_for_Andrey"))
         else:
             List.append(i)
 
