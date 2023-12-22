661ce0fae4f838566ed4dd0a1c53d192c2248c21
LordReaps <BovenkoAndrey@gmail.com>
2023-12-22 12:01:14 +0300

    Добавил рандомное выпадение карт Верхние 3 - расклад на ближайшее будущее Нижние 3 - расклад на дальнее будущее

--- data/random.py
+++ data/random.py
@@ -4,15 +4,12 @@ import os
 
 def RandomCard():
     List = []
-    while len(List)<=3:
+    while len(List)<=6:
         i = random.choice(os.listdir("static/tarot_for_Andrey"))
         if i in List:
             i = random.choice(os.listdir("static/tarot_for_Andrey"))
         else:
             List.append(i)
 
-    a = List[0]
-    b = List[1]
-    c = List[2]
-    return [a, b, c]
+    return List
 
