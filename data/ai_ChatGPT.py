9182b8411a476c8f7c8e6c93c77d5ab52639c9b8
KiraArensky <kshi.txt@yandex.ru>
2023-12-25 16:47:24 +0300

    loading v1.0

--- data/ai_ChatGPT.py
+++ data/ai_ChatGPT.py
@@ -2,13 +2,16 @@ import openai
 
 
 def ai_request(request_mess):
-    openai.api_key = "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"  # Ключ api ChatGPT
+    try:
+        openai.api_key = "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"  # Ключ api ChatGPT
 
-    completion = openai.ChatCompletion.create(  # Запрос
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": request_mess} # Сообщение для GPT
-        ]
-    )
-    print("\n\nЗапрос отправлен\n\n")
-    return completion.choices[0].message.content
+        completion = openai.ChatCompletion.create(  # Запрос
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": request_mess} # Сообщение для GPT
+            ]
+        )
+        print("\n\nЗапрос отправлен\n\n")
+        return completion.choices[0].message.content
+    except:
+        ai_request(request_mess)
